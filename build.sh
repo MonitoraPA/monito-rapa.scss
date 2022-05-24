540ca065d3bd8786227859839f2aeb8fa4fb9856
Massimo Maria Ghisalberti <zairik@gmail.com>
2022-05-24 11:57:40 +0200

    parameter font type

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sass ./light.scss:./css/monito-rapa-light.css --style=compressed
-sass ./dark.scss:./css/monito-rapa-dark.css --style=compressed
+sass ./light.scss:./css/monito-rapa-light.css
+sass ./dark.scss:./css/monito-rapa-dark.css
